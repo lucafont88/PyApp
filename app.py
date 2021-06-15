6f9ae235e4e118a35ff5eabfc3e2d9bbe7977a85
Luca Fontana <lucafont88@gmail.com>
2021-06-15 17:39:36 +0200

    wip test main loop

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from datetime import datetime
 import pandas as pd
 from streamlit_lib.track_focus import FocusTracker
+import time
 
 # Creating the Dashboard app #
 st.title("Focus Tracker")
@@ -13,3 +14,15 @@ df_focus, today = focus_tracker.create_focus_df()
 fig = focus_tracker.plotFocus(df_focus)
 
 st.write(fig)
+
+
+
+# # Creating the Dashboard app #
+# st.title("Test loop")
+
+# counter = 0
+
+# while True:
+#     counter = counter + 1
+#     st.write("<h1>" + str(counter) + "</h1>", unsafe_allow_html=True)
+#     time.sleep(2.0)
