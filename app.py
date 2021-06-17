c892d837ee4ca877d598e54de57a0ea1dbf9efd0
Luca Fontana <lucafont88@gmail.com>
2021-06-17 16:54:32 +0200

    wip class refactor and record key events

--- app.py
+++ app.py
@@ -3,31 +3,33 @@ from datetime import datetime
 import pandas as pd
 from streamlit_lib.track_text import TextTracker
 import time
-from typing import List
-from keyboard import KeyboardEvent
-
-_is_running = False
-_any_run = False
 
 st.title("Text Tracker")
 
-text_tracker = TextTracker()
 tracked_data_df: pd.DataFrame = {}
 
-if _is_running == True and st.button("Stop"):
-    text_tracker.stop_listening_keyboard()
-    tracked_data_df = text_tracker.get_tracked_key_events_df()
-    _is_running = False
+_signal_stop = st.button("Stop")
+_signal_start = st.button("Start")
 
-if _is_running == False and st.button("Start"):
-    _any_run = True
-    _is_running = True
-    text_tracker.start_listening_keyboard()
-    
-if _any_run == True:
-    st.write(tracked_data_df)
-# else:
-#     st.write("No Data")
+st.write("Results:")
+
+if _signal_stop == True:
+    TextTracker.stop_listening_keyboard()
+
+if _signal_start == True:
+    TextTracker.start_listening_keyboard()
+
+try:
+    tracked_data_df = TextTracker.load_keyboard_events_tracked_df()
+except:
+    tracked_data_df = TextTracker.get_empty_keyboard_events_tracked_df()
+
+st.write(tracked_data_df)
+
+
+
+
+
 
 # fig = focus_tracker.plotFocus(df_focus)
 
--- streamlit_lib/track_text.py
+++ streamlit_lib/track_text.py
@@ -1,36 +1,53 @@
 
 import keyboard
-from typing import List
 import pandas as pd
-
+import pathlib
 
 
 class TextTracker:
 
-    key_event_tracked: list
+    # Potrebbe essere inutile   
+    def logFocusTime(data_storage, start, end):
+        if not pathlib.Path(data_storage).is_file():
+            with open(data_storage, "w+") as focus:
+                focus.write(f"{start} {end}")
+                focus.write("\n")
+        else:    
+            with open(data_storage, "a") as focus:
+                focus.write(f"{start} {end}")
+                focus.write("\n")
 
-
-    def __init__(self):
-        self.key_event_tracked = []
-        #self._key_func = lambda self, keyEvent: self.key_event_tracked.append(keyEvent)
-
-    def start_listening_keyboard(self):
+    def start_listening_keyboard(data_storage = "./focus.txt"):
         #keyboard.on_release(lambda k:self.text_tracked.append(k))
         # Record events until 'esc' is pressed.
-        self.key_event_tracked = keyboard.record(until='esc')
+        key_event_tracked = keyboard.record(until='esc')
+        key_event_tracked_df = TextTracker._get_tracked_key_events_df(key_event_tracked)
+        key_event_tracked_df.to_csv(data_storage)
+        # keyboard.start_recording()
 
-    def stop_listening_keyboard(self):
+    def load_keyboard_events_tracked_df(data_storage = "./focus.txt"):
+        path = pathlib.Path(data_storage)
+        if path.exists() and path.is_file():
+            return pd.read_csv(data_storage) 
+    
+    def get_empty_keyboard_events_tracked_df():
+                return pd.DataFrame({
+            'key_names': [],
+            'key_codes': [],
+            'key_time': []
+        })
+
+    def stop_listening_keyboard():
         keyboard.send(hotkey='esc')
+
     # def stop_listening_keyboard(self):
     #     keyboard.unhook_all()
 
-    def get_raw_tracked_key_events(self) -> List[keyboard.KeyboardEvent]:
-        return self.key_event_tracked
 
-    def get_tracked_key_events_df(self) -> pd.DataFrame:
-        _tracked_keys_names = list(map(lambda k: k.name, self.key_event_tracked))
-        _tracked_keys_codes = list(map(lambda k: k.scan_code, self.key_event_tracked))
-        _tracked_keys_times = list(map(lambda k: k.time, self.key_event_tracked))
+    def _get_tracked_key_events_df(key_event_tracked) -> pd.DataFrame:
+        _tracked_keys_names = list(map(lambda k: k.name, key_event_tracked))
+        _tracked_keys_codes = list(map(lambda k: k.scan_code, key_event_tracked))
+        _tracked_keys_times = list(map(lambda k: k.time, key_event_tracked))
 
         return pd.DataFrame({
             'key_names': _tracked_keys_names,
